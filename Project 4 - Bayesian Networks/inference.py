c07d7d794669f5a2a97c2ec2548b72bda3ae3279
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2017-06-03 12:38:20 +0100

    Pass question 1

--- Project 4 - Bayesian Networks/inference.py
+++ Project 4 - Bayesian Networks/inference.py
@@ -148,22 +148,19 @@ class ExactInference(InferenceModule):
         emissionModel = busters.getObservationDistribution(noisyDistance)
         pacmanPosition = gameState.getPacmanPosition()
 
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-
         # Replace this code with a correct observation update
         # Be sure to handle the "jail" edge case where the ghost is eaten
         # and noisyDistance is None
-        allPossible = util.Counter()
-        for p in self.legalPositions:
-            trueDistance = util.manhattanDistance(p, pacmanPosition)
-            if emissionModel[trueDistance] > 0:
-                allPossible[p] = 1.0
+        if noisyDistance is None:
+            for p in self.legalPositions:
+                self.beliefs[p] = 0.0
+            self.beliefs[self.getJailPosition()] = 1.0
+        else:
+            for p in self.legalPositions:
+                trueDistance = util.manhattanDistance(p, pacmanPosition)
+                self.beliefs[p] *= emissionModel[trueDistance]
 
-        "*** END YOUR CODE HERE ***"
-
-        allPossible.normalize()
-        self.beliefs = allPossible
+            self.beliefs.normalize()
 
     def elapseTime(self, gameState):
         """
